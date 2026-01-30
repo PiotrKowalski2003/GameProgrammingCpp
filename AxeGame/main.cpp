284023f3dd667fb0530773d79ad0fbd576b53984
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-01-30 12:40:31 +0100

    04.Comparison Operators

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -2,9 +2,23 @@
 
 int main() {
 
-    float root_beer = 1.99;
-    double cheese_burger {5.99};
-    bool shouldHaveLunch {};
+    // false
+    bool equal {4 == 9};
+
+    // true
+    bool notEqual {4 != 9};
+
+    // true
+    bool less {4 < 9};
+
+    // false
+    bool greater {4 > 9};
+
+    // true
+    bool lessEqual {4 <= 9};
+
+    // false
+    bool greaterEqual {4 >= 9};
 
     const int width = 350;
 
