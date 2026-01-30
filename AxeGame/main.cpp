1264b20f3fcdc08ad607cff44ca02beafa25193e
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-01-30 12:43:38 +0100

    05.Loops

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -2,29 +2,13 @@
 
 int main() {
 
-    // false
-    bool equal {4 == 9};
-
-    // true
-    bool notEqual {4 != 9};
-
-    // true
-    bool less {4 < 9};
-
-    // false
-    bool greater {4 > 9};
-
-    // true
-    bool lessEqual {4 <= 9};
-
-    // false
-    bool greaterEqual {4 >= 9};
-
     const int width = 350;
 
     InitWindow(width, 200, "Init Window");
 
+    while (true) {
 
+    }
 
 
     return 0;
