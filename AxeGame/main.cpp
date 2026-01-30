43aaac015def8f17cdd16758517cb4cc3e7dc4d2
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-01-30 12:57:39 +0100

    09.Moving the Circle

--- AxeGame/main.cpp
+++ AxeGame/main.cpp
@@ -6,12 +6,27 @@ int main() {
 
     InitWindow(width, 200, "Init Window");
 
+    // circle coordinates
+    int circle_x {175};
+    int circle_y {100};
+
+    SetTargetFPS(60);
     while (WindowShouldClose() == false) {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        DrawCircle(175, 100, 25, BLUE);
+        // Game logic begins
 
+        DrawCircle(circle_x, circle_y, 25, BLUE);
+
+        if (IsKeyDown(KEY_D)) {
+            circle_x += 5;
+        }
+        if (IsKeyDown(KEY_A)) {
+            circle_x -= 5;
+        }
+
+        // Game logic ends
         EndDrawing();
     }
 
