aed1267d39d03d9e52dcf6619904dd56819f4554
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-01-31 12:23:40 +0100

    21.Setting Up main.cpp

--- DapperDasher/main.cpp
+++ DapperDasher/main.cpp
@@ -2,7 +2,24 @@
 
 int main() {
 
-    
+    // window dimensions
+    constexpr int windowWidth {512};
+    constexpr int windowHeight {380};
+    // initialize the window
+    InitWindow(windowWidth, windowHeight, "Dapper Dasher!");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose()) {
+        // start drawing
+        BeginDrawing();
+        ClearBackground(WHITE);
+
+
+        // stop drawing
+        EndDrawing();
+    }
+    CloseWindow();
+
 
     return 0;
 }
