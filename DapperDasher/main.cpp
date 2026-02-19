4c492f087247e0130dd1be04352b684d2fc55b01
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-02-19 21:49:18 +0100

    42.Scrolling Background

--- DapperDasher/main.cpp
+++ DapperDasher/main.cpp
@@ -88,6 +88,10 @@ int main()
 
     int velocity{0};
 
+    Texture2D background = LoadTexture("../textures/far-buildings.png");
+    float bgX{};
+
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -97,6 +101,12 @@ int main()
         // start drawing
         BeginDrawing();
         ClearBackground(WHITE);
+
+        bgX -= 20 * dT;
+
+        // draw the background
+        Vector2 bgPos{bgX, 0.0};
+        DrawTextureEx(background, bgPos, 0.0, 2.0, WHITE);
 
         // perform ground check
         if (isOnGround(scarfyData, windowDimensions[1]))
@@ -153,5 +163,6 @@ int main()
     }
     UnloadTexture(scarfy);
     UnloadTexture(nebula);
+    UnloadTexture(background);
     CloseWindow();
 }
