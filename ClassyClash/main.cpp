d7d65dcbadf748b50c8baed59f4fe9bdca081abe
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-02-20 20:49:34 +0100

    64.Enemy Class

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -2,6 +2,7 @@
 #include "raymath.h"
 #include "Character/Character.h"
 #include "Prop/Prop.h"
+#include "Character/Enemy.h"
 
 int main() {
 
@@ -18,6 +19,12 @@ int main() {
     Prop props[2] {
         Prop{Vector2{600.f, 300.f}, LoadTexture("../nature_tileset/Rock.png")},
         Prop{Vector2{400.f, 500.f}, LoadTexture("../nature_tileset/Log.png")},
+    };
+
+    Enemy goblin{
+        Vector2{},
+        LoadTexture("../characters/goblin_idle_spritesheet.png"),
+        LoadTexture("../characters/goblin_run_spritesheet.png"),
     };
 
     SetTargetFPS(60);
@@ -51,6 +58,8 @@ int main() {
                 knight.undoMovement();
             }
         }
+
+        goblin.tick(GetFrameTime());
 
         EndDrawing();
     }
