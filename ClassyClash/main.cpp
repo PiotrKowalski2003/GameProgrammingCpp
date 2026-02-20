f934a08edbc2759681882197ca0a926a3e1efc41
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-02-20 13:53:01 +0100

    56.The Tick Function

--- ClassyClash/main.cpp
+++ ClassyClash/main.cpp
@@ -4,10 +4,12 @@
 class Character {
 public:
     Vector2 getWorldPos(){return worldPos;}
+    void setScreenPos(int winWidth, int winHeight);
+    void tick(float deltaTime);
 private:
-    Texture2D texture;
-    Texture2D idle;
-    Texture2D run;
+    Texture2D texture{LoadTexture("../characters/knight_idle_spritesheet.png")};
+    Texture2D idle{LoadTexture("../characters/knight_idle_spritesheet.png")};
+    Texture2D run{LoadTexture("../characters/knight_run_spritesheet.png")};
     Vector2 screenPos;
     Vector2 worldPos;
     // 1 : facing right, -1 : facing left
@@ -17,8 +19,42 @@ private:
     int frame{};
     const int maxFrames{6};
     const float updateTime{1.f/12.f};
+    const float speed{4.0};
 };
 
+void Character::setScreenPos(int winWidth, int winHeight){
+    screenPos = {
+        (float)winWidth/2.0f - 4.0f*(0.5f * (float)texture.width/6.0f),
+        (float)winHeight/2.0f - 4.0f*(0.5f * (float)texture.height)
+    };
+
+}
+
+void Character::tick(float deltaTime){
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A)) direction.x -= 1.0;
+    if (IsKeyDown(KEY_D)) direction.x += 1.0;
+    if (IsKeyDown(KEY_W)) direction.y -= 1.0;
+    if (IsKeyDown(KEY_S)) direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0) {
+        // set worldPos = worldPos + direction
+
+        worldPos = Vector2Add(worldPos, Vector2Scale( Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+
+    } else {
+        texture = idle;
+    }
+
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime) {
+        frame++;
+        runningTime = 0.f;
+        if (frame >= maxFrames) frame = 0;
+    }
+}
 
 int main() {
 
@@ -53,21 +89,7 @@ int main() {
         BeginDrawing();
         ClearBackground(WHITE);
 
-        Vector2 direction{};
-        if (IsKeyDown(KEY_A)) direction.x -= 1.0;
-        if (IsKeyDown(KEY_D)) direction.x += 1.0;
-        if (IsKeyDown(KEY_W)) direction.y -= 1.0;
-        if (IsKeyDown(KEY_S)) direction.y += 1.0;
-        if (Vector2Length(direction) != 0.0) {
-            // set mapPos = mapPos - direction
 
-            mapPos = Vector2Subtract(mapPos, Vector2Scale( Vector2Normalize(direction), speed));
-            direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-            knight = knight_run;
-
-        } else {
-            knight = knight_idle;
-        }
 
 
         // draw the map
