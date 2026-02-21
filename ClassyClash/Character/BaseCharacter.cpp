b961b4f6c986424137ce40e04726e2302f0ff330
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-02-21 13:05:40 +0100

    66.Inheriting from BaseCharacter

--- ClassyClash/Character/BaseCharacter.cpp
+++ ClassyClash/Character/BaseCharacter.cpp
@@ -3,3 +3,15 @@
 BaseCharacter::BaseCharacter() {
 
 }
+
+void BaseCharacter::undoMovement() {
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle BaseCharacter::getCollisionRec() {
+    return Rectangle{
+        screenPos.x,
+        screenPos.y,
+        scale * width,
+        scale * height};
+}
