a0cb736593f1dfc948edda285cdc8823906fdbf6
Piotr Kowalski <167914684+PiotrKowalski2003@users.noreply.github.com>
2026-02-21 13:14:47 +0100

    67.Overriding Functions

--- ClassyClash/Character/BaseCharacter.cpp
+++ ClassyClash/Character/BaseCharacter.cpp
@@ -15,3 +15,20 @@ Rectangle BaseCharacter::getCollisionRec() {
         scale * width,
         scale * height};
 }
+
+void BaseCharacter::tick(float deltaTime) {
+    worldPosLastFrame = worldPos;
+
+    // update animation frame
+    runningTime += deltaTime;
+    if (runningTime >= updateTime) {
+        frame++;
+        runningTime = 0.f;
+        if (frame >= maxFrames) frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
